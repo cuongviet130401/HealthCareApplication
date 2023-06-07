356b81d9b8ba2263da3aa7ef050d75ba28d96783
manhahahihi <91709638+manhahahihi@users.noreply.github.com>
2023-06-07 11:09:02 +0700

    update back to homeAct

--- app/src/main/java/com/example/healthcareapplication/FindDoctorActivity.java
+++ app/src/main/java/com/example/healthcareapplication/FindDoctorActivity.java
@@ -1,8 +1,10 @@
 package com.example.healthcareapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.content.Intent;
 
 public class FindDoctorActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class FindDoctorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_doctor);
+
+        TextView backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FindDoctorActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
